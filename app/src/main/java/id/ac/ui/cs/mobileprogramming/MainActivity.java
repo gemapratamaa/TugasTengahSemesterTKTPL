ffe175c733c81da3c0c56212d3be6ee60284c2c2
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-11-20 01:41:00 +0700

    change layout for http get request

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
@@ -2,16 +2,32 @@ package id.ac.ui.cs.mobileprogramming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.entity.mime.Header;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mPriceTextView;
+
+    private String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
     }
-
-
-
 }
