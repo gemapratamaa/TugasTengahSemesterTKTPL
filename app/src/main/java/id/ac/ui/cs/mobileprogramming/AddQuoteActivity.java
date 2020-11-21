f3e0fe77604296ff1f624e269b39465b75a8a5c2
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-11-21 14:21:02 +0700

    added more icons and new empty activity to add quotes

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/AddQuoteActivity.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.mobileprogramming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddQuoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_quote);
+    }
+}
