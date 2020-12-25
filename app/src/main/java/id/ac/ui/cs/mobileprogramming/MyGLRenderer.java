56bd518b4cce43c7baa054b0f0cf230bfff64f5a
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-12-25 22:07:47 +0700

    updated manifest

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/MyGLRenderer.java
@@ -0,0 +1,37 @@
+package id.ac.ui.cs.mobileprogramming;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+
+    private Triangle triangle;
+
+    private final float[] vPMatrix = new float[16];
+    private final float[] projectionMatrix = new float[16];
+    private final float[] viewMatrix = new float[16];
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+        GLES20.glClearColor(0.0f,0.0f,0.0f,1.0f);
+        triangle = new Triangle();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 unused) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        Matrix.setLookAtM(viewMatrix, 0,0,0,-3,0f,0f,0f);
+    }
+}
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/MyGLSurfaceView.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.mobileprogramming;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class MyGLSurfaceView extends GLSurfaceView {
+
+    private final MyGLRenderer renderer;
+
+    public MyGLSurfaceView(Context context) {
+        super(context);
+
+        setEGLContextClientVersion(2);
+
+        renderer = new MyGLRenderer();
+
+        setRenderer(renderer);
+    }
+}
