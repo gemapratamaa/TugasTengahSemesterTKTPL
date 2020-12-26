9bc71338a39e6d6ed3d257bb0113257145a91399
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-12-26 21:54:00 +0700

    add notif, blm dites

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +26,8 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
     private NotificationManagerCompat notificationManager;
+    int notificationId = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +37,8 @@ public class MainActivity extends AppCompatActivity {
         Button quoteButton = findViewById(R.id.button_random_quote);
         Button catButton = findViewById(R.id.button_random_cat_picture);
         Button reminder = findViewById(R.id.button_reminder);
+        TextView noConnectionText = findViewById(R.id.no_connection_text);
         notificationManager = NotificationManagerCompat.from(this);
-
 
 
         reminder.setOnClickListener(v -> {
@@ -43,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Hourly quote set!", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(MainActivity.this, NotificationBroadcast.class);
             PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this,id, intent,0);
-
             AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
             //long timeAtButtonClick = System.currentTimeMillis();
@@ -67,16 +69,38 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        // fitur no 9
         if (!isConnectedToInternet()) {
             quoteButton.setVisibility(View.GONE);
             catButton.setVisibility(View.GONE);
+            noConnectionText.setText("Not connected to internet! Please connect to the internet and " +
+                            "try again.");
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, 8)
+                .setSmallIcon(R.drawable.bubblespeech)
+                .setContentTitle("Notice")
+                .setContentText("You have put the app to the background")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+
+        notificationManager.notify(notificationId, builder.build());
+        notificationId++;
+
     }
 
     private boolean isConnectedToInternet() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
+
 
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -92,5 +116,25 @@ public class MainActivity extends AppCompatActivity {
             notificationManager.createNotificationChannel(channel);
         }
     }
+
+
+
+    /*
+    private void createNotificationChannel(String notifName) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = notifName;
+            String description = notifName;
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel channel = new NotificationChannel("backgroundNotif", name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+     */
 
 }
